887c1e46ec023a6b3e4192bd75db91960a27c5af
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2024-05-31 22:03:28 -0400

    Start main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# install nltk for TextBlob
+
+from TextBlob import TextBlob
+# import data for analysis
+
+# for sentiment analysis, sentiment between range [-1, 1]
+# as value approaches 1, more positive; vice versa
